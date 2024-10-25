becf57d3738f5a20b68d51fe8bc419e48d90d3ac
Jared <jaredzheng@vectorbuilder.net>
2024-10-25 21:57:26 +0800

    currencyExchange

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"exchangeapp/config"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	config.InitConfig()
 	fmt.Println("Hello, World!")
-	fmt.Println(config.AppConfig.App.Port)
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
 }
