5864450703ebcb37fe474920e9c771fdd81e631a
Jared <jaredzheng@vectorbuilder.net>
2024-10-28 00:33:33 +0800

    1

--- controllers/auth.go
+++ controllers/auth.go
@@ -9,14 +9,11 @@ import (
 
 func Register(ctx *gin.Context) {
 	var user models.User
-	if err := ctx.ShouldBindJSON(&user); err != nil {
+	err := ctx.ShouldBindJSON(&user)
+	if err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{
 			"error": err.Error(),
 		})
 		return
 	}
-
-	ctx.JSON(200, gin.H{
-		"message": "register success",
-	})
 }
