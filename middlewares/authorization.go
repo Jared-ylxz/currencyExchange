2c177a61b06e1521838ae18e68511d0763537f0c
Jared <jaredzheng@vectorbuilder.net>
2024-10-30 00:11:12 +0800

    1

--- middlewares/authorization.go
+++ middlewares/authorization.go
@@ -1,1 +1,20 @@
 package middlewares
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		if inputToken := ctx.GetHeader("Authorization"); inputToken != "" {
+			ctx.JSON(http.StatusUnauthorized, gin.H{
+				"message": "Authorization header is missing",
+			})
+			ctx.Abort() // stop the chain of handlers
+			return
+		}
+		// username, err := utils.ParseJWT(inputToken)
+	}
+}
