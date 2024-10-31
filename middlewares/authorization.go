71c8adbb060d403b8c527b7018841cd9e232ceb9
jared <jaredzheng@vectorbuilder.net>
2024-10-31 13:37:43 +0800

    1

--- middlewares/authorization.go
+++ middlewares/authorization.go
@@ -1,6 +1,7 @@
 package middlewares
 
 import (
+	"exchangeapp/utils"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -8,15 +9,24 @@ import (
 
 func AuthMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		if inputToken := ctx.GetHeader("Authorization"); inputToken != "" {
+		inputToken := ctx.GetHeader("Authorization")
+		if inputToken == "" {
 			ctx.JSON(http.StatusUnauthorized, gin.H{
 				"message": "Authorization header is missing",
 			})
 			ctx.Abort() // 只执行这一个中间件，不再执行后续的中间件（如有多个中间件）
 			return
 		}
-		// username, err := utils.ParseJWT(inputToken)
 
-		// if err!= nil {}
+		username, err := utils.ParseJWT(inputToken)
+		if err != nil {
+			ctx.JSON(http.StatusUnauthorized, gin.H{
+				"message": "Invalid token",
+			})
+			ctx.Abort()
+			return
+		}
+		ctx.Set("username", username) // 将用户名存入上下文
+		ctx.Next()                    // 执行后续的中间件
 	}
 }
